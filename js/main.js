e01d2fc2b7155630f3f5e88826d881f1b557d9dd
Anton Trochuk <trochuk19@gmail.com>
2024-11-04 20:22:01 +0400

    Исправление замечаний

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-const min = 1;
-const max = 25;
-const minLikes = 1;
-const maxLikes = 25;
-const minComments = 0;
-const maxComments = 30;
+const MIN = 1;
+const MAX = 25;
+const MIN_LIKES = 1;
+const MAX_LIKES = 25;
+const MIN_COMMENTS = 0;
+const MAX_COMMENTS = 30;
 const randomDescription = ['На отдыхе', 'На даче', 'Рыбалка', 'Соревнования'];
 const message = ['Всё отлично!','В целом всё неплохо. Но не всё.','Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.','Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.','Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.','Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 const nameComments = ['Антон', 'Егор', 'Иван', 'Аня', 'Ева', 'Саша'];
 
 const commentsDescription = function () {
   const comments = [];
-  const commentsCount = Math.round(Math.random() * (maxComments - minComments + 1)) + minComments;
+  const commentsCount = Math.round(Math.random() * (MAX_COMMENTS - MIN_COMMENTS + 1)) + MIN_COMMENTS;
   for (let i = 0; i < commentsCount; i++) {
     comments.push({
       id: i + 1,
-      avatar: `img/avatar-${ Math.round(Math.random() * (max - min + 1)) + min }.jpg`,
+      avatar: `img/avatar-${ Math.round(Math.random() * (MAX - MIN + 1)) + MIN }.jpg`,
       message: message[Math.floor(Math.random() * message.length)],
       name: nameComments[Math.floor(Math.random() * nameComments.length)],
     });
@@ -22,21 +22,21 @@ const commentsDescription = function () {
   return comments;
 };
 
-const object = function () {
-  const objectFoto = [];
+const dataGeneration = function () {
+  const photoInformation = [];
   const objectFotoCount = 25;
   for (let i = 0; i < objectFotoCount; i++) {
-    objectFoto.push({
-      id: Math.round(Math.random() * (max - min + 1)) + min,
-      url: `photos/${ Math.round(Math.random() * (max - min + 1)) + min }.jpg`,
+    photoInformation.push({
+      id: Math.round(Math.random() * (MAX - MIN + 1)) + MIN,
+      url: `photos/${ Math.round(Math.random() * (MAX - MIN + 1)) + MIN }.jpg`,
       description: randomDescription[Math.floor(Math.random() * randomDescription.length)],
-      likes: Math.round(Math.random() * (maxLikes - minLikes + 1)) + minLikes,
+      likes: Math.round(Math.random() * (MAX_LIKES - MIN_LIKES + 1)) + MIN_LIKES,
       comments: commentsDescription ()
     });
   }
 
-  return objectFoto;
+  return photoInformation;
 };
-console.log (object());
+console.log (dataGeneration());
 
 
