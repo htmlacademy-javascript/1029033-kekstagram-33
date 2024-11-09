58e65e8d0c2cf6c343f4868c23bea43199d1bb94
Anton Trochuk <trochuk19@gmail.com>
2024-11-09 15:59:34 +0400

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-function lengthCheck (line, maxLength) {
+/*function lengthCheck (line, maxLength) {
 
   if (line.length <= maxLength) {
     return true;
@@ -19,4 +19,41 @@ function isPalindrome (line) {
     return false;
   }
 }
-isPalindrome('анна');
+isPalindrome('анна'); */
+
+
+function timeForBusiness (startDay, endDay, startMeeting, duration) {
+  const startDaySplit = startDay.split(':');
+  const endDaySplit = endDay.split(':');
+  const startMeetingSplit = startMeeting.split(':');
+  let hour = 0;
+  let minutes = 0;
+
+  for (let i = 1; (i + 60) - i <= duration; i++) {
+    duration = duration - 60;
+    hour = i;
+    minutes = duration;
+  }
+
+  const hourStartDay = Number(startDaySplit[0]);
+  const minutesStartDay = Number(startDaySplit[1]);
+
+  const hourEndDay = Number(endDaySplit[0]);
+  const minutesEndDay = Number(endDaySplit[1]);
+
+  const hourStartMeeting = Number(startMeetingSplit[0]);
+  const minutesStartMeeting = Number(startMeetingSplit[1]);
+
+  if (hourStartMeeting >= hourStartDay) {
+    if ((hourStartMeeting + hour) <= hourEndDay && (minutesStartMeeting + minutes) <= minutesEndDay) {
+      console.log('true');
+    } else {
+      console.log('false');
+    }
+  } else {
+    console.log('false');
+  }
+
+
+}
+timeForBusiness('14:00', '17:30', '08:0', 90);
