d616ec9f89469b7b3f3cf187a5714a63bd4d48ed
Anton Trochuk <trochuk19@gmail.com>
2024-11-23 20:59:02 +0400

    добавление Pristine

--- js/adding-new-photo.js
+++ js/adding-new-photo.js
@@ -2,6 +2,18 @@ const body = document.querySelector('body');
 const openForm = document.querySelector('.img-upload__overlay.hidden');
 const uploadInput = document.getElementById('upload-file');
 const closeForm = document.getElementById('upload-cancel');
+const uploadForm = document.querySelector('.img-upload__form');
+const submitButton = document.querySelector('.img-upload__submit');
+const textHashtags = document.querySelector('.text__hashtags');
+const textDescription = document.querySelector('.text__description');
+
+
+const pristineConfig = {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--error',
+};
+
 
 const clickOpenForm = function () {
   uploadInput.addEventListener('change', () => {
@@ -26,5 +38,8 @@ const closeEventListeners = function () {
     }
   });
 };
+
+const pristine = new Pristine(uploadForm, pristineConfig, false);
+
 
 export {clickOpenForm, clickCloseForm, closeEventListeners};
